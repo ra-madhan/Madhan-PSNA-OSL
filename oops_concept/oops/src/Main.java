86933e05864d41a432d114dd0b2c47e2e0279553
ra-madhan <ramadhan062007@gmail.com>
2026-08-19 15:35:18 +0530

    Access modifier

--- oops_concept/oops/src/Main.java
+++ oops_concept/oops/src/Main.java
@@ -5,18 +5,30 @@ enum orderStatusS{
 public  class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        Order order=new Order(123,orderStatusS.REFUNDED);
+        Order order=new Order(123,orderStatusS.REFUNDED,989);
+        //order.orderPrice=9684154;
+        //order.setOrderprice(9999999);
     }
 }
 
 class Order {
     public  int orderId;
     public orderStatusS orderStatus;
+    private int orderPrice;
 
-    Order(int orderId,orderStatusS orderStatus){
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    Order(int orderId, orderStatusS orderStatus, int orderPrice){
         this.orderId=orderId;
         this.orderStatus=orderStatus;
-        System.out.println("Your order id:"+orderId+" is "+orderStatus);
+        this.orderPrice=orderPrice;
+        System.out.println("Your order id:"+orderId+" is "+orderStatus +" value is "+orderPrice);
 
     }
 }
