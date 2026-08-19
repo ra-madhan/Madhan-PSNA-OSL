ccbfca5af0997e0d6d575af4a153d55cdcf49577
ra-madhan <ramadhan062007@gmail.com>
2026-08-19 12:22:10 +0530

    Create OrderStatus Field

--- oops_concept/oops/src/Main.java
+++ oops_concept/oops/src/Main.java
@@ -3,9 +3,21 @@
 public  class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        Order order=new Order(123,"Pending");
     }
 }
 class Order {
+    int orderId;
+    String orderStatus;
+    Order(int orderId){
+        this.orderId=orderId;
+        System.out.println("orderId:"+orderId);
 
+    }
+    Order(int orderId,String orderStatus ){
+        this(orderId);
+        this.orderStatus=orderStatus;
+        System.out.println("Order status:"+orderStatus);
+    }
 
 }
