0fae9a5c729993c35b2011085c3894ce289314dc
ra-madhan <ramadhan062007@gmail.com>
2026-08-19 15:13:51 +0530

    Addded enum

--- oops_concept/oops/src/Main.java
+++ oops_concept/oops/src/Main.java
@@ -1,16 +1,19 @@
 
-
+enum orderStatusS{
+    PENDING,SHIPPED,DELIVERED,CANCELLED,REFUNDED
+}
 public  class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        Order order=new Order(123,"Pending");
+        Order order=new Order(123,orderStatusS.REFUNDED);
     }
 }
+
 class Order {
     public  int orderId;
-    public String orderStatus;
-    
-    Order(int orderId,String orderStatus){
+    public orderStatusS orderStatus;
+
+    Order(int orderId,orderStatusS orderStatus){
         this.orderId=orderId;
         this.orderStatus=orderStatus;
         System.out.println("Your order id:"+orderId+" is "+orderStatus);
